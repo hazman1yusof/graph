211170a0cab8f10741c73ec9c68a1febb9c68e3a
hazman1yusof <hazman.yusof@gmail.com>
2018-08-13 02:02:23 +0800

    takde apap2 la

--- util.js
+++ util.js
@@ -82,10 +82,11 @@ function tick_yaxis(max,min,nbsp,placeholder1){
 	return arr;
 }
 
-function updRange(min,max){
+function updRange(min,max,range){
 	$("#customRange2").attr('min',min);
 	$("#customRange2").attr('max',max);
-	$('#rangeshow').text($("#customRange2").val());
+	$("#customRange2").val(range);
+	$('#rangeshow').text(range);
 	$('#rangemax').text(max);
 }
 
@@ -96,7 +97,7 @@ function filterRange(array,range){
 function plot_pain_score(plot,array){
 	array.forEach(function(item,index){
 		let o = plot.pointOffset({ x: index+1, y: 23});
-		$("#placeholder").append("<div style='position:absolute;left:" + (o.left-18) + "px;top:" + o.top + "px;color:#666;font-size:smaller'>"+item[1]+"</div>");
+		$("#placeholder").append("<div style='position:absolute;left:" + (o.left-10) + "px;top:" + o.top + "px;color:#666;font-size:smaller'>"+item[1]+"</div>");
 	});
 }
 
@@ -109,7 +110,14 @@ function plot_time_date(plot,array){
 	});
 }
 
-function markings(){
+function plot_total(plot,array){
+	array.forEach(function(item,index){
+		let o = plot.pointOffset({ x: index+1, y: 0.2});
+		$("#placeholder2").append("<div style='position:absolute;left:" + (o.left-10) + "px;top:" + o.top + "px;color:#666;font-size:smaller'><b>"+item[1]+"</b></div>");
+	});
+}
+
+function markings1(){
 	var markings = [
 		{ color: "#f6f6f6", yaxis: { from: 180 } },
 		{ color: "#f6f6f6", yaxis: { to: 30 } },
@@ -118,6 +126,33 @@ function markings(){
 	];
 
 	return markings;
+}
+
+function markings2(){
+	var markings = [
+		{ color: "#f6f6f6", yaxis: { from: 14.5} },
+		{ color: "#f6f6f6", yaxis: { to: 0.5} },
+		{ color: "#000", lineWidth: 2, yaxis: { from: 11.5, to: 11.5 } },
+		{ color: "#000", lineWidth: 2, yaxis: { from: 6.5, to: 6.5 } },
+		{ color: "#000", lineWidth: 2, yaxis: { from: 0.5, to: 0.5 } },
+	];
+
+	return markings;
+}
+
+function accum_more_tick(array,accum){
+	array = array.map(function(item,index){
+		return [item[0],parseInt(item[1])+accum];
+	});
+	return array;
+}
+
+function total_tick(tick1,tick2,tick3){
+	let total = tick1.map(function(item,index){
+		return [index,parseInt(item[1])+parseInt(tick2[index][1])+parseInt(tick3[index][1])];
+	});
+
+	return total
 }
 
 function formatTime(array){
@@ -129,9 +164,8 @@ function formatTime(array){
 	return array;
 }
 
-function legend_tepi(plot){
+function legend_tepi1(plot){
 	let o = plot.pointOffset({ x: 0, y: 120});
-	// let bp = "Blood Pressure<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class='sistole'>(BLUE)</span>";
 	let bp=`<div class="row" >
 				<div class="col-3">
 					<b>Blood Pressure</b></br>
@@ -148,14 +182,21 @@ function legend_tepi(plot){
 			</div>`;
 
 	$("#placeholder").append("<div class='legendtepi' style='top:" + o.top + "px;'>"+bp+"</div>");
+}
 
+function legend_tepi2(plot){
+	let o = plot.pointOffset({ x: 0, y: 11});
+	let bp=`<div class="row" >
+				<div class="col-3">
+					<b>Best Motor Response</b>
+				</div>
+				<div class="col-3">
+					<b>Best Verbal Response</b>
+				</div>
+				<div class="col-3">
+					<b>Best tick3 Response</b>
+				</div>
+			</div>`;
 
-	// o = plot.pointOffset({ x: 0, y: 100});
-	// let hr = "Heart Rate<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class='pulse'>(RED)</span>";
-	// $("#placeholder").append("<div class='legendtepi' style='top:" + o.top + "px;'>"+hr+"</div>");
-
-
-	// let o = plot.pointOffset({ x: 0, y: 90});
-	// let bp = "Blood Pressure<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class='sistole'>(Blue)</span>";
-	// $("#placeholder").append("<div class='legendtepi' style='top:" + o.top + "px;'>"+bp+"</div>");
+	$("#placeholder2").append("<div class='legendtepi' style='top:" + (o.top-25) + "px;left:-270px;'>"+bp+"</div>");
 }
